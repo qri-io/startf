18f678bab9065686836b7457cd1a324fb44684c0
b5 <sparkle_pony_2000@qri.io>
2019-05-31 10:08:57 -0400

    chore(ci): update circleci to gomodules, cleanup gx artifacts

--- .circleci/cover.test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-echo "" > coverage.txt
-go test -v -race -coverprofile=profile.out -covermode=atomic github.com/qri-io/startf
-if [ -f profile.out ]; then
-  cat profile.out >> coverage.txt
-  rm profile.out
-fi
--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 
 update-changelog:
 	conventional-changelog -p angular -i CHANGELOG.md -s
+
+test:
+	go test ./... -v --coverprofile=coverage.txt --covermode=atomic
